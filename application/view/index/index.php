828512b29418a8962a035f2cbfe2e3fd3919435b
iindigoo <indigobaloe@gmail.com>
2016-06-22 19:35:26 +0200

    pics

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -1,16 +1,16 @@
 <div class="content">
 	<div class="box">
 		<div class="row">
-			<div class="col-md-4 col-md-4-content"> <img src="dist/img/icons/png/coin-1.png" alt=""> 
+			<div class="col-md-4 col-md-4-content"> <img src="<?php echo Config::get('URL'); ?>images/coin-1.png" alt=""> 
 				<h1>Play Game</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga et non sint repudiandae dicta cupiditate obcaecati beatae libero optio sunt modi facilis, reiciendis quam dolorem cumque fugiat magnam laborum laboriosam.</p>
 			</div>
-			<div class="col-md-4 col-md-4-content"> <img src="_pictures/money.png" alt=""> 
+			<div class="col-md-4 col-md-4-content"> <img src="<?php echo Config::get('URL'); ?>images/money.png" alt=""> 
 				<h1>Earn money</h1> 
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil iste, praesentium, asperiores saepe consectetur veritatis nulla atque esse aspernatur eum eligendi id, sed assumenda repudiandae minus quo? Illo, deserunt, in!</p>
 			</div>
 			<div class="col-md-4 col-md-4-content"> 
-            <img src="pictures/pig.png" alt=""> 
+            <img src="<?php echo Config::get('URL'); ?>images/pig.png" alt=""> 
 				<h1>Withdraw</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero cum soluta veniam odio optio illum quos maiores aspernatur eaque atque consequuntur quisquam ipsum impedit nisi repellendus, aliquam commodi repellat nulla.</p>
 			</div>
