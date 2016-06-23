8ae344e3d3244d3b7fed106407684ddef29e1fb0
Dino Bijdevier <99033225@mydavinci.nl>
2016-06-23 13:38:18 +0200

    Tickets
    lelijk maar ik weet niks

--- application/view/tickets/index.php
+++ application/view/tickets/index.php
@@ -1,10 +1,22 @@
-<body>
+<body class="ticketsBackground">
 
-<div class="container">
+<div class="container" id="tickets">
+
+<p>wait {SECONDS} for 20 tickets!</p>
+	<div class="progress">
+	  <div class="progress-bar" style="width: 1%;"></div>
+	</div>
+
+
 	<div class="panel panel-default">
+		<div class="panel panel-default" id="hmm">
+	  <div class="panel-body">
+	  </div>
+	</div>
 	  <div class="panel-body">
 
 	  </div>
 	</div>
+
 </div>
 </body>
