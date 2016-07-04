e9c0d52a4efdf1b403a064ee0c34df2e0755cfd9
Dino Bijdevier <99033225@mydavinci.nl>
2016-07-04 18:23:24 +0200

    header
    header aangepast!

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -41,7 +41,9 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class=""><a href="<?php echo Config::get('URL'); ?>tickets/index">Free tickets</a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Roulette</a></li>
+        <li><a href="#">Depoist</a></li>
+        <li><a href="#">Market</a></li>
       </ul>
      
       <ul class="nav navbar-nav navbar-right">
