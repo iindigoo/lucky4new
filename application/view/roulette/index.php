3c34c1b93d5b555df1b67c0bc12d258d1d899751
iindigoo <indigobaloe@gmail.com>
2016-08-30 22:13:27 +0200

    k

--- application/view/roulette/index.php
+++ application/view/roulette/index.php
@@ -1,40 +1,6 @@
-
 <div class="container">
-	<div id="spin_uptions" class="holder_spin">
-
-		<div class="color red"><div class="name">10</div></div>
-		<div class="color black"><div class="name">4</div></div>
-		<div class="color red"><div class="name">7</div></div>
-		<div class="color black"><div class="name">18</div></div>
-		<div class="color red"><div class="name">9</div></div>
-		<div class="color black"><div class="name">13</div></div>
-		<div class="color red"><div class="name">20</div></div>
-		<div class="color black"><div class="name">6</div></div>
-		<div class="color red"><div class="name">2</div></div>
-		<div class="color black"><div class="name">16</div></div>
-		<div class="color gold"><div class="name">0</div></div>
-		<div class="color red"><div class="name">5</div></div>
-		<div class="color black"><div class="name">19</div></div>
-		<div class="color red"><div class="name">15</div></div>
-		<div class="color black"><div class="name">12</div></div>
-		<div class="color red"><div class="name">17</div></div>
-		<div class="color black"><div class="name">1</div></div>
-		<div class="color red"><div class="name">8</div></div>
-		<div class="color black"><div class="name">3</div></div>
-		<div class="color red"><div class="name">11</div></div>
-		<div class="color black"><div class="name">14</div></div>
-
-	</div>
-	<div id="move_me" class="klicker"></div>
-		<a onclick="move();" href="#">Spin me</a>
-
-		<div>
-		<div id="bett" class="black"></div>
-		<div id="bett" class="gold"></div>
-		<div id="bett" class="red"></div>
-	</div>
-
-	<div class="chatbox">
-		
+	<div class="row">
+	  <div class="chatbox col-md-3">.col-md-9 .col-md-push-3</div>
+	  <div class="col-md-9">.col-md-3 .col-md-pull-9</div>
 	</div>
 </div>
